3e3d3f4aab698ef270a060aa5e2c8ed40e6d21c4
Callum Elder <cjames.02@icloud.com>
2025-01-05 11:05:26 +1000

    removed redundant code

--- src/back-end/transcribe.py
+++ src/back-end/transcribe.py
@@ -108,67 +108,67 @@ class MicrophoneStream:
             yield b"".join(data)
 
 
-def listen_print_loop(responses: object) -> str:
-    """Iterates through server responses and prints them.
+# def listen_print_loop(responses: object) -> str:
+#     """Iterates through server responses and prints them.
 
-    The responses passed is a generator that will block until a response
-    is provided by the server.
+#     The responses passed is a generator that will block until a response
+#     is provided by the server.
 
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
+#     Each response may contain multiple results, and each result may contain
+#     multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+#     print only the transcription for the top alternative of the top result.
 
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
+#     In this case, responses are provided for interim results as well. If the
+#     response is an interim one, print a line feed at the end of it, to allow
+#     the next result to overwrite it, until the response is a final one. For the
+#     final one, print a newline to preserve the finalized transcription.
 
-    Args:
-        responses: List of server responses
+#     Args:
+#         responses: List of server responses
 
-    Returns:
-        The transcribed text.
-    """
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
+#     Returns:
+#         The transcribed text.
+#     """
+#     num_chars_printed = 0
+#     for response in responses:
+#         if not response.results:
+#             continue
 
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
-        result = response.results[0]
-        if not result.alternatives:
-            continue
+#         # The `results` list is consecutive. For streaming, we only care about
+#         # the first result being considered, since once it's `is_final`, it
+#         # moves on to considering the next utterance.
+#         result = response.results[0]
+#         if not result.alternatives:
+#             continue
 
-        # Display the transcription of the top alternative.
-        transcript = result.alternatives[0].transcript
+#         # Display the transcription of the top alternative.
+#         transcript = result.alternatives[0].transcript
 
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = " " * (num_chars_printed - len(transcript))
+#         # Display interim results, but with a carriage return at the end of the
+#         # line, so subsequent lines will overwrite them.
+#         #
+#         # If the previous result was longer than this one, we need to print
+#         # some extra spaces to overwrite the previous result
+#         overwrite_chars = " " * (num_chars_printed - len(transcript))
         
-        if result.is_final:
-            break
+#         if result.is_final:
+#             break
 
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + "\r")
-            sys.stdout.flush()
+#         if not result.is_final:
+#             sys.stdout.write(transcript + overwrite_chars + "\r")
+#             sys.stdout.flush()
 
-            num_chars_printed = len(transcript)
+#             num_chars_printed = len(transcript)
 
-        else:
-            print(transcript + overwrite_chars)
+#         else:
+#             print(transcript + overwrite_chars)
 
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                print("Exiting..")
-                break
+#             # Exit recognition if any of the transcribed phrases could be
+#             # one of our keywords.
+#             if re.search(r"\b(exit|quit)\b", transcript, re.I):
+#                 print("Exiting..")
+#                 break
 
-            num_chars_printed = 0
+#             num_chars_printed = 0
 
-    return transcript
+#     return transcript
--- src/back-end/workflow.py
+++ src/back-end/workflow.py
@@ -187,6 +187,5 @@ if __name__ == "__main__":
     
     # Setup and run the graph
     graph = setup_graph()
-    print("Ready")
     state = MessagesState(messages=[])
     state = graph.invoke(state)
